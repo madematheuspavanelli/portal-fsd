99df0f9561a6a22044a99e23225cced498522e28
Matheus Pavanelli <matheus.pavanelli@madeinweb.app>
2025-05-14 11:06:59 -0300

    fix: update ESLint configuration for Vue and TypeScript, add new dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,16 @@
 import vue from "eslint-plugin-vue";
+import parserVue from "vue-eslint-parser";
 import typescript from "@typescript-eslint/eslint-plugin";
-import parser from "@typescript-eslint/parser";
+import parserTs from "@typescript-eslint/parser";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default [
   {
-    files: ["**/*.ts", "**/*.vue"],
-    ignores: ["dist", "node_modules"],
+    files: ["**/*.vue"],
     languageOptions: {
-      parser,
+      parser: parserVue,
       parserOptions: {
+        parser: parserTs,
         ecmaVersion: "latest",
         sourceType: "module",
         extraFileExtensions: [".vue"],
@@ -22,13 +23,25 @@ export default [
     },
     rules: {
       "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-      "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-      "@typescript-eslint/no-explicit-any": "warn",
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        { argsIgnorePattern: "^_" },
-      ],
       "vue/multi-word-component-names": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: parserTs,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: {
+      "@typescript-eslint": typescript,
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
     },
