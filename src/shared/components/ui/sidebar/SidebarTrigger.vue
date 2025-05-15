645b5337c02ad9c5b9fbaeaf79edbf1c22bc9827
matheuspavanelli <mspavanelli@gmail.com>
2025-05-14 23:46:03 -0300

    feat: add basic ui components

---
+++ src/shared/components/ui/sidebar/SidebarTrigger.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { PanelLeft } from "lucide-vue-next";
+import type { HTMLAttributes } from "vue";
+
+import { Button } from "@/shared/components/ui/button";
+import { cn } from "@/shared/lib/utils";
+
+import { useSidebar } from "./utils";
+
+const props = defineProps<{
+  class?: HTMLAttributes["class"];
+}>();
+
+const { toggleSidebar } = useSidebar();
+</script>
+
+<template>
+  <Button
+    data-sidebar="trigger"
+    data-slot="sidebar-trigger"
+    variant="ghost"
+    size="icon"
+    :class="cn('h-7 w-7', props.class)"
+    @click="toggleSidebar"
+  >
+    <PanelLeft />
+    <span class="sr-only">Toggle Sidebar</span>
+  </Button>
+</template>
---
+++ src/shared/components/ui/tags-input/TagsInputItem.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import {
+  TagsInputItem,
+  type TagsInputItemProps,
+  useForwardProps,
+} from "reka-ui";
+import { computed, type HTMLAttributes } from "vue";
+
+import { cn } from "@/shared/lib/utils";
+
+const props = defineProps<
+  TagsInputItemProps & { class?: HTMLAttributes["class"] }
+>();
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props;
+
+  return delegated;
+});
+
+const forwardedProps = useForwardProps(delegatedProps);
+</script>
+
+<template>
+  <TagsInputItem
+    v-bind="forwardedProps"
+    :class="
+      cn(
+        'bg-secondary data-[state=active]:ring-ring ring-offset-background flex h-5 items-center rounded-md data-[state=active]:ring-2 data-[state=active]:ring-offset-2',
+        props.class,
+      )
+    "
+  >
+    <slot />
+  </TagsInputItem>
+</template>
