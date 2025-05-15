421c3a5ca855eb4e1cf542cdf7da34a736a0a48d
matheuspavanelli <mspavanelli@gmail.com>
2025-05-15 00:00:59 -0300

    feat: initialize axios instance with base URL

--- src/app/providers/vue-query.ts
+++ src/app/providers/vue-query.ts
@@ -1,7 +1,15 @@
 import { QueryClient, VueQueryPlugin } from "@tanstack/vue-query";
 import type { App } from "vue";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+});
+
 export function setupVueQuery(app: App) {
-  const queryClient = new QueryClient();
   app.use(VueQueryPlugin, { queryClient });
 }
